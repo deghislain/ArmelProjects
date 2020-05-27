35f24d81d286e9ea45d0ed4d695c92a63d974d7d
deghislain <deghislain@gmail.com>
2020-05-27 14:02:25 -0400

    fixed the navigation issue. improved the vie

--- ParallelAutoMapRedSystem/WebContent/index.jsp
+++ ParallelAutoMapRedSystem/WebContent/index.jsp
@@ -1,23 +1,24 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
-<%-- <%@ taglib prefix="sf" uri = "http://www.springframework.org/tags/form"%> --%>
+
 <%@ taglib prefix="c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
     
 <html>
 <head>
-
+<%@page isELIgnored = "false" %>
 <meta charset="ISO-8859-1">
 <title>Parallel Automated Map Reduce System</title>
 </head>
 <body>
 
 	<div style="align-content: center; border-color: #333333; width: 70%; height: 635px; border-style: solid; border-width: 1px; padding-left: 400px;">
-		<div style="padding-top: 20px"></div>
+		<div style="padding-top: 20px; padding-left: 300px">
 		<div
-			style="align-content: center; border-color: #333333; width: 60%; height: 435px; border-style: solid; border-width: 5px; padding-top: 10px; border-color: blue">
+			style="align-content: center; border-color: #333333; width: 600px; height: 435px; border-style: solid; border-width: 5px; padding-top: 10px; border-color: blue; position:absolute;Right:600px;">
 			<H2 style="color: red">Web Application Performing Automated MapReduce</H2>
-			<form method="Post" enctype="multipart/form-data" action="paraMapRedCont/upload">
+			<form method="Post" enctype="multipart/form-data" action="upload">
 					<div style="padding-top: 5px">
 						<h4 style="color:maroon;">Status: ${status}</h4>
 						<H3>File Upload</H3>
@@ -26,7 +27,7 @@
 						<input type="submit" value="Upload" name="fUpload"/>
 					</div>
 			</form>
-			<form method="post" action="paraMapRedCont/partition">
+			<form method="post" action="partition">
 					<div>
 						<H3>Choose N, the number of parallel threads. N >= 1</H3>
 						<label>Number of Threads</label> 
@@ -36,7 +37,7 @@
 						<input type="submit" value="Perform MapReduce Computation" style="color: blue">
 					</div>
 			</form>
-			<form method="get" action="paraMapRedCont/display">
+			<form method="get" action="display">
 					<div style="padding-top: 40px; padding-left: 250px">
 						<input type="submit" value="Display Results" style="color: green">
 					</div>
@@ -44,10 +45,10 @@
 			<div>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" style="position:absolute;right:250px;">
     <div style="padding-top:20px;" >
-        <div style="align-content: center; border-color: #333333; width: 60%; height: 120px; border-style: solid; border-width: 1px; overflow:scroll">
-            <h3 style="color:red; padding-left: 250px">Results: ${message}</h3>
+        <div style="align-content: center; border-color: #333333; width: 350px; height: 435px; border-style: solid; border-width: 1px; overflow:scroll;">
+            <h4 style="color:red; padding-left: 35px">Results: ${message}</h4>
             <table style="align-content: center; border-color: #333333; width: 50%">
             	<tr>
             		<th style="border-style: solid;">Word</th>
@@ -60,6 +61,7 @@
         </div>
 
         </div>
+    </div>
     </div>
 	</div>
 </body>
--- ParallelAutoMapRedSystem/src/com/perso/proj/mapred/controller/MapRedController.java
+++ ParallelAutoMapRedSystem/src/com/perso/proj/mapred/controller/MapRedController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
@@ -13,7 +14,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,7 +31,7 @@ import com.perso.proj.mapred.services.TaskTrackerService;
  * Servlet implementation class MapRedController
  */
 @Controller
-@RequestMapping("/paraMapRedCont")
+@RequestMapping("/")
 public class MapRedController {
 	private static final long serialVersionUID = 1L;
 	protected final Log logger = LogFactory.getLog(getClass());
@@ -51,8 +52,8 @@ public class MapRedController {
 		this.msg = "";
 	}
 
-	@RequestMapping(value ="/upload", method = RequestMethod.POST)
-	public String uploadFile(HttpServletRequest req, Model model) {
+	@RequestMapping(value ="upload", method = RequestMethod.POST)
+	public ModelAndView uploadFile(HttpServletRequest req, HttpServletResponse resp, ModelMap model) {
 		List<FileItem> formItems = processUploadFile(req, model);
 		if (null != formItems && !formItems.isEmpty()) {
 			String uploadPath = req.getServletContext().getRealPath("/") + UPLOAD_DIRECTORY;
@@ -62,11 +63,11 @@ public class MapRedController {
 			this.status = "File Upload Error";
 			model.addAttribute("status", this.status);
 		}
-
-		return "/index";
+		ModelAndView mv = new ModelAndView("index", model);
+		return mv;
 	}
 
-	private List<FileItem> processUploadFile(HttpServletRequest request, Model model) {
+	private List<FileItem> processUploadFile(HttpServletRequest request, ModelMap model) {
 		List<FileItem> formItems = null;
 		// configures upload settings
 		DiskFileItemFactory factory = new DiskFileItemFactory();
@@ -87,7 +88,7 @@ public class MapRedController {
 	
 	
 	@RequestMapping(value = "partition", method = RequestMethod.POST)
-public String performMapReduce(HttpServletRequest req, Model model) {
+public ModelAndView performMapReduce(HttpServletRequest req, ModelMap model) {
 	List<List<String>> parts = this.doDataPartition(req, model);
 	//TODO create threads
 	
@@ -96,9 +97,11 @@ public String performMapReduce(HttpServletRequest req, Model model) {
 	ITaskTrackerService tts = new TaskTrackerService();
 	JobTrackerThread jtt = new JobTrackerThread(mrbs, tts, this.nService, uploadPath);
 	jtt.run();
-	return "/index";
+	
+	ModelAndView mv = new ModelAndView("index", model);
+	return mv;
 }
-	private List<List<String>> doDataPartition(HttpServletRequest req, Model model) {
+	private List<List<String>> doDataPartition(HttpServletRequest req, ModelMap model) {
 		String numThread = req.getParameter("numThread");
 		List<List<String>> words = null;
 		String uploadPath = req.getServletContext().getRealPath("/") + UPLOAD_DIRECTORY;
@@ -129,7 +132,7 @@ public String performMapReduce(HttpServletRequest req, Model model) {
 	}
 	
 	@RequestMapping(value = "display", method = RequestMethod.GET)
-	public String displayResults(HttpServletRequest req, Model model) {
+	public ModelAndView displayResults(HttpServletRequest req, ModelMap model) {
 		String uploadPath = req.getServletContext().getRealPath("/") + UPLOAD_DIRECTORY;
 		HashMap<String, String> resultMap = this.nService.displayResults(uploadPath);
 		if(resultMap != null && !resultMap.isEmpty()) {
@@ -140,7 +143,8 @@ public String performMapReduce(HttpServletRequest req, Model model) {
 			this.msg = "Nothing To Display, Make Sure You Have Uploaded A File And Perform A Map Reduce";
 			model.addAttribute("message", this.msg);
 		}
-		return "/index";
+		ModelAndView mv = new ModelAndView("index", model);
+		return mv;
 	}
 
 }
